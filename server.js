3431f5b264e8e18592b305270961c4aacba01b4e
Alexander Bosikov <legionerab3000@yandex.ru>
2022-10-24 17:39:27 +0300

    Add assignment port to process.env

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-
-// app.get('/', (req, res) => res.send('Hello World!'));
+const PORT = (process.env.PORT = 3000);
 
 app.use(express.static(__dirname + '/dist'));
 
